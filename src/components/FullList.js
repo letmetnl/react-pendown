da104ed1a19d2d966cb26da691eec577246267ee
mohits Projects <mohit5150515@gmail.com>
2020-09-18 03:47:21 +0530

    final updates

--- src/components/FullList.js
+++ src/components/FullList.js
@@ -3,18 +3,26 @@ import React, { Component } from "react";
 import Item from "./Item";
 export default class FullList extends Component {
   render() {
-    const { items } = this.props;
+    const { items, clearList, handleDelete, handleEdit } = this.props;
 
     return (
       <ul className="list-group my-5">
         <h3 className="text-capitalize text-center">Your Notes</h3>
         {items.map((item) => {
-          return <Item key={item.id} title={item.title} />;
+          return (
+            <Item
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
         })}
 
         <button
           type="button"
           className="btn btn-danger btn-block text-capitalize mt-5"
+          onClick={clearList}
         >
           Clear Full-List
         </button>
