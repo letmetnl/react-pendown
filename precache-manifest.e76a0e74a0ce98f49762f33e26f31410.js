e08c54bfaaef00cb1934a85eaffc4257e1fc9c76
mohits Projects <mohit5150515@gmail.com>
2020-09-18 04:03:05 +0530

    Updates

---
+++ precache-manifest.e76a0e74a0ce98f49762f33e26f31410.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a8eba70c6791c23522cba58ebbd9fc",
+    "url": "/react-pendown/index.html"
+  },
+  {
+    "revision": "f1cfc023f9117f3cc4d5",
+    "url": "/react-pendown/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b7874b9646d344eb3236",
+    "url": "/react-pendown/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f1cfc023f9117f3cc4d5",
+    "url": "/react-pendown/static/js/2.c33e272f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pendown/static/js/2.c33e272f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7874b9646d344eb3236",
+    "url": "/react-pendown/static/js/main.47461215.chunk.js"
+  },
+  {
+    "revision": "19a78d6f3f68fddb9677",
+    "url": "/react-pendown/static/js/runtime-main.cd493a51.js"
+  }
+]);
