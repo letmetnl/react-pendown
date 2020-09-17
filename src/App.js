b7b630759dac2fed686a32640cba61deb2fb9076
mohits Projects <mohit5150515@gmail.com>
2020-09-18 01:52:50 +0530

    added bootstrap and uuid and updated app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import TaskInput from "./components/TaskInput";
 import FullList from "./components/FullList";
 // using no custom css just bootstrap so commenting this
 // import "./App.css";
-
+// importing bootstrap deendency installed
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
 export default class App extends Component {
   render() {
     return (
-      <div>
-        hello there!
-        <TaskInput />
-        <FullList />
+      <div className="container">
+        <div className="row">
+          <div className="col col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">
+              Pen Down Your Thought
+            </h3>
+            <TaskInput />
+            <FullList />
+          </div>
+        </div>
       </div>
     );
   }
