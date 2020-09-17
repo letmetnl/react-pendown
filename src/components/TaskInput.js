dab6160f8a3d7f4c3d19019f26761f7ecb29cad3
mohits Projects <mohit5150515@gmail.com>
2020-09-18 03:39:06 +0530

    updated edit and delete item

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class TaskInput extends Component {
   render() {
-    const { item, handleChange, handleSubmit } = this.props;
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
       <div className="card card-body my-3">
         <form onSubmit={handleSubmit}>
